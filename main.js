e8e998bd43ecef6df9a7137cbbc61511e3728616
Gmedulla101 <edosegheosamagbe@gmail.com>
2023-11-10 08:33:17 +0100

    Finalisnig the functionality of the web app.

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-const getWeather = async () => {
-    const response = await fetch("")
+const getWeather = async (cityName) => {
+    const apiKey = "d8781c91cd5807e099ef7078a40ecfe6"
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
+    const weatherData = await response.json();
+    console.log(weatherData);
+
+    //DYNAMICALLY CHANGING THE CONTENTS OF THE WEATHER INFORMATION
+    document.querySelector(".city-name").textContent = weatherData.sys.country + ", " + weatherData.name;
+    document.querySelector(".temp").textContent = weatherData.main.temp + "°C"
+    document.querySelector(".humidity-value").textContent = weatherData.main.humidity;
+    document.querySelector(".wind-value").textContent = weatherData.wind.speed;
+    const weatherImg = document.querySelector(".weather-img img");
+    if(weatherData.weather[0].main === "Clear") {
+        weatherImg.src = "images/clear.png";
+    } else if (weatherData.weather[0].main === "Clouds") {
+        weatherImg.src = "images/clouds.png";
+    } else if (weatherData.weather[0].main === "Rain") {
+        weatherImg.src = "images/rain.png";
+    } else if (weatherData.weather[0].main === "Drizzle") {
+        weatherImg.src = "images/drizzle.png";
+    } else if (weatherData.weather[0].main === "Mist") {
+        weatherImg.src = "images/mist.png";
+    }
 }
+
+const searchBtn = document.querySelector(".search-button");
+console.log(searchBtn);
+searchBtn.addEventListener('click', (event) => {
+    cityName = document.querySelector(".search-bar").value;
+    getWeather(cityName);
+})
