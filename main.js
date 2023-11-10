8c9e1c31d37308bbc7ee54955734eef29bfd5ac5
Gmedulla101 <edosegheosamagbe@gmail.com>
2023-11-10 08:37:55 +0100

    Deleted unnecesary code

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const getWeather = async (cityName) => {
     const apiKey = "d8781c91cd5807e099ef7078a40ecfe6"
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
     const weatherData = await response.json();
-    console.log(weatherData);
 
     //DYNAMICALLY CHANGING THE CONTENTS OF THE WEATHER INFORMATION
     document.querySelector(".city-name").textContent = weatherData.sys.country + ", " + weatherData.name;
     document.querySelector(".temp").textContent = weatherData.main.temp + "°C"
-    document.querySelector(".humidity-value").textContent = weatherData.main.humidity;
-    document.querySelector(".wind-value").textContent = weatherData.wind.speed;
+    document.querySelector(".humidity-value").textContent = weatherData.main.humidity + "%";
+    document.querySelector(".wind-value").textContent = weatherData.wind.speed + " Km/h";
+
+    //DYNAMICALLY CHANGING THE IMAGES
     const weatherImg = document.querySelector(".weather-img img");
     if(weatherData.weather[0].main === "Clear") {
         weatherImg.src = "images/clear.png";
@@ -24,7 +25,6 @@ const getWeather = async (cityName) => {
 }
 
 const searchBtn = document.querySelector(".search-button");
-console.log(searchBtn);
 searchBtn.addEventListener('click', (event) => {
     cityName = document.querySelector(".search-bar").value;
     getWeather(cityName);
